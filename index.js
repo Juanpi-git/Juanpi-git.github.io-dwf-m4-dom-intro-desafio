ca3cda4a866590a1291f54492a2b9909edc16424
Juanpi <juan.pablo.pendenza.22@gmail.com>
2024-05-18 16:07:40 -0300

    Desafio terminado

--- index.js
+++ index.js
@@ -25,6 +25,25 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  //selecciono la lista mediante su clase(.lista), y los items dentro de ella mediante su etiqueta(li)
+  const ulEl = document.querySelector(".lista");
+  const liEl = document.querySelectorAll("li");
+  console.log(ulEl);
+  console.log(liEl);
+
+  //elimino los items dentro de la lista
+  liEl.forEach((e) => {
+    ulEl.removeChild(e);
+  });
+
+  //creo los nuevos items
+  for (let i = 0; i < cosasQueAprendimos.length; i++) {
+    const newLiEl = document.createElement("li");
+    newLiEl.textContent = cosasQueAprendimos[i].tema;
+    newLiEl.setAttribute("class", `${cosasQueAprendimos[i].class}`);
+    ulEl.appendChild(newLiEl);
+  }
+}
 
 main();
